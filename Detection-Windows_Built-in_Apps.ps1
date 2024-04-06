373ab64161c172fda966672eb5a88983ac77e4f7
Joymalya Basu Roy <86624602+jbasuroy369@users.noreply.github.com>
2024-04-06 17:21:13 +0530

    Add files via upload

---
+++ Detection-Windows_Built-in_Apps.ps1
@@ -0,0 +1,42 @@
+﻿try 
+{
+    # Array of built-in apps to remove
+    $UninstallPackages = @(
+	    "Microsoft.549981C3F5F10",
+	    "Microsoft.BingNews",
+	    "Microsoft.GamingApp",
+	    "Microsoft.GetHelp",
+	    "Microsoft.Getstarted",
+	    "Microsoft.MicrosoftOfficeHub",
+	    "Microsoft.MicrosoftSolitaireCollection",
+	    "Microsoft.People",
+	    "Microsoft.WindowsCommunicationsApps",
+	    "Microsoft.Xbox.TCUI",
+	    "Microsoft.XboxGameOverlay",
+	    "Microsoft.XboxGamingOverlay",
+	    "Microsoft.XboxIdentityProvider",
+	    "Microsoft.XboxSpeechToTextOverlay",
+	    "Microsoft.YourPhone",
+	    "MicrosoftTeams",
+	    "Microsoft.WindowsFeedbackHub"
+        )
+
+    $InstalledPackages = Get-AppxPackage -AllUsers | Where {($UninstallPackages -contains $_.Name)}
+    $ProvisionedPackages = Get-AppxProvisionedPackage -Online | Where {($UninstallPackages -contains $_.DisplayName)}
+
+    if($InstalledPackages -eq $null -and $ProvisionedPackages -eq $null){
+        Write-Host "Remediation not needed"
+        Exit 0
+    }
+    else
+    {
+        Write-Host "Remediation needed"
+        Exit 1
+    }
+}
+catch{
+    $errMsg = $_.Exception.Message
+    Write-Host $errMsg
+    Exit 1
+}
+
---
+++ Remediation-Windows_Built-in_Apps.ps1
@@ -0,0 +1,39 @@
+﻿# Array of built-in apps to remove
+$UninstallPackages = @(
+	    "Microsoft.549981C3F5F10",
+	    "Microsoft.BingNews",
+	    "Microsoft.GamingApp",
+	    "Microsoft.GetHelp",
+	    "Microsoft.Getstarted",
+	    "Microsoft.MicrosoftOfficeHub",
+	    "Microsoft.MicrosoftSolitaireCollection",
+	    "Microsoft.People",
+	    "Microsoft.WindowsCommunicationsApps",
+	    "Microsoft.Xbox.TCUI",
+	    "Microsoft.XboxGameOverlay",
+	    "Microsoft.XboxGamingOverlay",
+	    "Microsoft.XboxIdentityProvider",
+	    "Microsoft.XboxSpeechToTextOverlay",
+	    "Microsoft.YourPhone",
+	    "MicrosoftTeams",
+	    "Microsoft.WindowsFeedbackHub"
+)
+
+$InstalledPackages = Get-AppxPackage -AllUsers | Where {($UninstallPackages -contains $_.Name)}
+$ProvisionedPackages = Get-AppxProvisionedPackage -Online | Where {($UninstallPackages -contains $_.DisplayName)}
+
+# Remove provisioned packages first
+ForEach ($ProvPackage in $ProvisionedPackages) {
+    Try {
+        $Null = Remove-AppxProvisionedPackage -PackageName $ProvPackage.PackageName -Online -ErrorAction Stop
+    }
+    Catch {Write-Warning -Message "Failed to remove provisioned package: [$($ProvPackage.DisplayName)]"}
+}
+
+# Remove appx packages
+ForEach ($AppxPackage in $InstalledPackages) {
+    Try {
+        $Null = Remove-AppxPackage -Package $AppxPackage.PackageFullName -AllUsers -ErrorAction Stop
+    }
+    Catch {Write-Warning -Message "Failed to remove Appx package: [$($AppxPackage.Name)]"}
+}
